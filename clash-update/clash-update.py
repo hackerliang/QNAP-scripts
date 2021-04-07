bbde0cec64b614ec931c8005a629ba4965dad72a
hackerliang <1254548379@qq.com>
2021-04-07 15:48:31 +0800

    clash-update: pep8

--- clash-update/clash-update.py
+++ clash-update/clash-update.py
@@ -6,23 +6,22 @@ import time
 import urllib.request
 import configparser
 
-
 config = configparser.ConfigParser()
 
 config.read('clash-update.ini')
-
 
 base_str = str()
 for key in config['SETTINGS']:
     base_str = base_str + "{}: {}\n".format(key, config['SETTINGS'][key])
 base_str = base_str + "\n\n"
 
-
-urlreq = urllib.request.Request(config["PROVIDER"]["URL"])
-urlreq.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36')
+url_req = urllib.request.Request(config["PROVIDER"]["URL"])
+url_req.add_header('User-Agent',
+                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+                   Chrome/87.0.4280.141 Safari/537.36')
 while True:
     try:
-        response = urllib.request.urlopen(urlreq, timeout=10)
+        response = urllib.request.urlopen(url_req, timeout=10)
         print("{} Download config success.".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
         con = response.read().decode('utf-8')
         num = con.index("proxies:")
@@ -40,11 +39,15 @@ while True:
               --body-data '{"path": "/root/.config/clash/config.yaml"}' '""" + config["SETTINGS"]["URL"] + """/configs'
         """)
         if cmd == 0:
-            print("{} Restart docker container success: {}.".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), config["CONTAINER"]["NAME"]))
+            print(
+                "{} Restart docker container success: {}.".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                                                                  config["CONTAINER"]["NAME"]))
         else:
-            print("{} Restart docker container failed: {}.".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), cmd))
+            print("{} Restart docker container failed: {}.".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                                                                   cmd))
         response.close()
     except Exception as e:
         print(e)
-    print("{} Sleep for {}s".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), config["PROVIDER"]["UPDATEINTERVAL"]))
+    print("{} Sleep for {}s".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                                    config["PROVIDER"]["UPDATEINTERVAL"]))
     time.sleep(int(config["PROVIDER"]["UPDATEINTERVAL"]))
